13126f2679a30efc481180a57f444e601c356fd1
Orazio <xabynix@gmail.com>
2023-10-06 14:20:35 +0000

    Update index.html and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,42 +48,43 @@ And here. | Okay. | I think we get it.
 `;
 
 function App() {
-	const [previewText, setPreviewText] = useState<string>(defaultText);
+  const [previewText, setPreviewText] = useState<string>(defaultText);
 
-	function handleOnChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-		setPreviewText(e.currentTarget.value);
-	}
+  function handleOnChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setPreviewText(e.currentTarget.value);
+  }
 
-	return (
-		<div id="container">
-			<div className="editorWrapper wrapperStyle">
-				<div className="toolbar">
-					<h4>Editor</h4>
-				</div>
-				<textarea
-					autoFocus
-					id="editor"
-					className="style"
-					onChange={(e) => handleOnChange(e)}
-				></textarea>
-			</div>
-			<div className="previewWrapper wrapperStyle">
-				<div className="toolbar">
-					<h4>Preview</h4>
-				</div>
-				<div
-					id="preview"
-					className="style"
-					dangerouslySetInnerHTML={{
-						__html: marked.parse(previewText, {
-							breaks: true,
-							gfm: true,
-						}),
-					}}
-				></div>
-			</div>
-		</div>
-	);
+  return (
+    <div id="container">
+      <div className="editorWrapper wrapperStyle">
+        <div className="toolbar">
+          <h4>Editor</h4>
+        </div>
+        <textarea
+          value={previewText}
+          autoFocus
+          id="editor"
+          className="style"
+          onChange={(e) => handleOnChange(e)}
+        ></textarea>
+      </div>
+      <div className="previewWrapper wrapperStyle">
+        <div className="toolbar">
+          <h4>Preview</h4>
+        </div>
+        <div
+          id="preview"
+          className="style"
+          dangerouslySetInnerHTML={{
+            __html: marked.parse(previewText, {
+              breaks: true,
+              gfm: true,
+            }),
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
